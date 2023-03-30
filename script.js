74598beb6109bfc580442609c9cebe0bd87c7bfa
Jishan040 <118433367+Jishan040@users.noreply.github.com>
2023-03-31 01:34:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -495,14 +495,4 @@ function won() {
     boardDivs.forEach(boardCell => {
         boardCell.removeEventListener("contextmenu", addingFlag)
     })
-} 
-document.getElementById('reset_button').addEventListener('click', reset);
-
-document.getElementById('change_button').addEventListener('click', () => {
-    reset();
-    const blockId = document.getElementById("block_id").value;
-    const color = document.getElementById("colour_id").value;
-    // alert(colorId)
-    const block = document.getElementById(`${blockId}`);
-    block.style.backgroundColor = color;
-});
+}
